85fc0b3cc04681e6681820db1719e61bb011859b
Arthur <arthur4456645@gmail.com>
2024-02-28 14:19:44 +0800

    build ProgressControl html & css

--- src/Components/ProgressControl/ProgressControl.jsx
+++ src/Components/ProgressControl/ProgressControl.jsx
@@ -2,6 +2,10 @@ import './ProgressControl.scss'
 
 export function ProgressControl () {
   return ( 
-    <></>
+    <div className="progress-control">
+      <button className='pre-button'><i className="fa-solid fa-arrow-left"></i> 上一步</button>
+      <button className='next-button'>下一步 <i className="fa-solid fa-arrow-right"></i></button>
+      <button className='order-button'>確認下單</button>  
+    </div>
    );
 }
